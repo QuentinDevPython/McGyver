98bec97d314f352970f230930d7da7c086aec67e
QuentinDevPython <quentin.barthelemy.01@gmail.com>
2020-09-28 14:23:26 +0200

    feat(maze) add the maze and the allowed player movements [McGyver-03]

--- lib/game.py
+++ lib/game.py
@@ -1,5 +1,6 @@
 import pygame
 from player import Player
+from maze import Maze
 
 # create a class that will represent the game
 
@@ -8,3 +9,4 @@ class Game:
 
     def __init__(self):
         self.player = Player()
+        self.maze = Maze()
--- lib/player.py
+++ lib/player.py
@@ -1,25 +1,50 @@
 import pygame
+from maze import Maze
 
 # create a class that represent the player
 
 
-class Player(pygame.sprite.Sprite):
+class Player():
 
     def __init__(self):
+        self.maze = Maze()
+        self.maze.create_maze()
         self.velocity = 40
-        self.image = pygame.image.load('assets/characters/mac_gyver.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = 1
-        self.rect.y = 1
+        self.player_image = pygame.image.load(
+            'assets/characters/mac_gyver.png')
+        self.player_image = pygame.transform.scale(self.player_image, (36, 38))
+        self.rect = self.player_image.get_rect()
+        self.rect.x = 40
+        self.rect.y = 40
 
     def move_right(self):
-        self.rect.x += self.velocity
+        if self.maze.is_free_square(1, 0):
+            self.rect.x += self.velocity
+            self.maze.player_position = (
+                self.maze.player_position[0] + 1, self.maze.player_position[1])
 
     def move_left(self):
-        self.rect.x -= self.velocity
+        if self.maze.is_free_square(-1, 0):
+            self.rect.x -= self.velocity
+            self.maze.player_position = (
+                self.maze.player_position[0] - 1, self.maze.player_position[1])
 
     def move_up(self):
-        self.rect.y -= self.velocity
+        if self.maze.is_free_square(0, -1):
+            self.rect.y -= self.velocity
+            self.maze.player_position = (
+                self.maze.player_position[0], self.maze.player_position[1] - 1)
 
     def move_down(self):
-        self.rect.y += self.velocity
+        if self.maze.is_free_square(0, 1):
+            self.rect.y += self.velocity
+            self.maze.player_position = (
+                self.maze.player_position[0], self.maze.player_position[1] + 1)
+
+    def is_victorious(self):
+        if self.maze.is_guardian_square():
+            print('Win')
+            return True
+        elif not self.maze.is_guardian_square():
+            print('Defeat')
+            pass
