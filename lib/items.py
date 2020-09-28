85759a9d8961bdc41ed1796bb1e9e65a0719cab5
QuentinDevPython <quentin.barthelemy.01@gmail.com>
2020-09-28 19:07:01 +0200

    feat(items) add items and the player inventory [McGyver-05]

---
+++ lib/items.py
@@ -0,0 +1,28 @@
+import pygame
+from player import Player
+
+
+class Items(pygame.sprite.Sprite):
+
+    def __init__(self, image_name, game):
+        super().__init__()
+        self.player = Player(game)
+        self.image = pygame.image.load('assets/items/' + image_name)
+
+        self.rect = self.image.get_rect()
+        print(self.image.get_rect())
+        self.item_position = self.position
+        self.rect.x = self.item_position[0]
+        self.rect.y = self.item_position[1]
+
+    def remove(self):
+        self.player.game.all_items.remove(self)
+
+    @property
+    def position(self):
+        item_position = self.player.maze.is_item_square
+
+        item_position = (
+            item_position[0] * 40, item_position[1] * 40)
+
+        return item_position
--- lib/player.py
+++ lib/player.py
@@ -4,18 +4,36 @@ from maze import Maze
 # create a class that represent the player
 
 
-class Player():
+class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
+        super().__init__()
         self.maze = Maze()
         self.maze.create_maze()
         self.velocity = 40
-        self.player_image = pygame.image.load(
+        self.image = pygame.image.load(
             'assets/characters/mac_gyver.png')
-        self.player_image = pygame.transform.scale(self.player_image, (36, 38))
-        self.rect = self.player_image.get_rect()
+        self.image = pygame.transform.scale(self.image, (36, 38))
+        self.rect = self.image.get_rect()
         self.rect.x = 40
         self.rect.y = 40
+        self.inventory = []
+
+    def is_collision(self):
+        position_player = (self.rect.x, self.rect.y)
+        if self.game.check_collision(self, self.game.all_items):
+            self.inventory.append('True')
+            print(self.game.item_needle.item_position)
+            print(position_player)
+            if position_player == self.game.item_needle.item_position:
+                self.game.all_items.remove(self.game.item_needle)
+            if position_player == self.game.item_tube.item_position:
+                self.game.all_items.remove(self.game.item_tube)
+            if position_player == self.game.item_syringe.item_position:
+                self.game.all_items.remove(self.game.item_syringe)
+            if position_player == self.game.item_ether.item_position:
+                self.game.all_items.remove(self.game.item_ether)
 
     def move_right(self):
         if self.maze.is_free_square(1, 0):
@@ -24,27 +42,32 @@ class Player():
                 self.maze.player_position[0] + 1, self.maze.player_position[1])
 
     def move_left(self):
+
         if self.maze.is_free_square(-1, 0):
             self.rect.x -= self.velocity
             self.maze.player_position = (
                 self.maze.player_position[0] - 1, self.maze.player_position[1])
 
     def move_up(self):
+
         if self.maze.is_free_square(0, -1):
             self.rect.y -= self.velocity
             self.maze.player_position = (
                 self.maze.player_position[0], self.maze.player_position[1] - 1)
 
     def move_down(self):
+
         if self.maze.is_free_square(0, 1):
             self.rect.y += self.velocity
             self.maze.player_position = (
                 self.maze.player_position[0], self.maze.player_position[1] + 1)
 
     def is_victorious(self):
-        if self.maze.is_guardian_square():
+        if self.maze.is_guardian_square() and self.inventory == ['True', 'True', 'True', 'True']:
             print('Win')
+            pygame.quit()
             return True
-        elif not self.maze.is_guardian_square():
+        elif self.maze.is_guardian_square():
             print('Defeat')
-            pass
+            pygame.quit()
+            return True
